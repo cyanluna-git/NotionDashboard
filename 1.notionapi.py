ffd0b2dd7a960f3392ed5e827574841b501a5eba
gerald park <51350627+cyanluna-git@users.noreply.github.com>
2023-02-07 07:00:18 +0900

    initial Commit

---
+++ 1.notionapi.py
@@ -0,0 +1,17 @@
+import requests
+
+TOKEN = "secret_duOD0wDCJn8pgrLcEasnRO5TOsOYu29hwikVYkBynoa"
+DATABASE_ID = "cfed17ffa07d4d52bfec6d9a46a8e92b"
+
+url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
+payload = {"page_size": 100}
+headers = {
+    "Authorization": "Bearer " + TOKEN,
+    "Notion-Version": "2022-06-28",
+    "content-type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code) 
