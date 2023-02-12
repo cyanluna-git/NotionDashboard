812981cb3043550e3f574f9acec0a4995cc73839
gerald park <51350627+cyanluna-git@users.noreply.github.com>
2023-02-13 07:41:37 +0900

    add get databases

--- 2.getpage.py
+++ 2.getpage.py
@@ -2,12 +2,12 @@ import requests
 
 TOKEN = "secret_duOD0wDCJn8pgrLcEasnRO5TOsOYu29hwikVYkBynoa"
 WORKITEM_DB = "cfed17ffa07d4d52bfec6d9a46a8e92b"
-MEMBER_DB = "95c5ea9b1e094dd9a35227958d5f6603"
-PROJECT_DB = "cfed17ffa07d4d52bfec6d9a46a8e92b"
-VERSION_DB = "cfed17ffa07d4d52bfec6d9a46a8e92b"
-KANBAN_DB = "cfed17ffa07d4d52bfec6d9a46a8e92b"
+MEMBER_DB = "9311c49e87c44fbf973d25995194808d"
+PROJECT_DB = "0cdbad552f24482a808aaa388b1187c4"
+VERSION_DB = "e5632cfff96048a4b6bd46ae014b3694"
+KANBAN_DB = "0e43a41b7aeb4b0e968a2b472b77f911"
 
-url = f"https://api.notion.com/v1/databases/{WORKITEM_DB}/query"
+
 
 payload = {"page_size": 100}
 headers = {
@@ -16,21 +16,26 @@ headers = {
     "content-type": "application/json"
 }
 
-def getPages():    
+def getDatabase(database, db_name):    
+    url = f"https://api.notion.com/v1/databases/{database}/query"
     response = requests.post(url, json=payload, headers=headers)
     data = response.json()
     
     import json
-    with open('db.json','w', encoding='utf8') as f:
+    with open( db_name + '.json','w', encoding='utf8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
     results = data["results"]
     return results
 
-pages = getPages()
+worktiems = getDatabase(WORKITEM_DB, 'workitem')
+members = getDatabase(MEMBER_DB, 'member')
+projects = getDatabase(PROJECT_DB, 'project')
+versions = getDatabase(VERSION_DB, 'version')
+kanbans = getDatabase(KANBAN_DB, 'kanban')
 
-for page in pages:
-    page_id = page["id"]
-    props = page["properties"]
-    title = props["Title"]
-    print(title)
+# for page in pages:
+#     page_id = page["id"]
+#     props = page["properties"]
+#     title = props["Title"]
+#     print(title)
